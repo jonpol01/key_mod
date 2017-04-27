d84705cdf5b71649f6dc44a1036d4c7a19d8d6b6
jonpol01 <johnpaul@soliva.me>
2017-04-27 18:32:19 +0900

    mod unstable

--- include/mod.hpp
+++ include/mod.hpp
@@ -22,10 +22,14 @@
 #include <ecl/config/ecl.hpp>
 #include <kobuki_msgs/DigitalOutput.h>
 
-//#include "packet_handler/payload_base.hpp"
+#include <ros/ros.h>
+#include "../../../_cart_core_/kobuki_driver/include/kobuki_driver/packet_handler/payload_base.hpp"
+//#include "../../../_cart_/kobuki_node/include/kobuki_node/kobuki_ros.hpp"
+
 //#include "modules/led_array.hpp"
 //#include "modules.hpp"
 //#include "macros.hpp"
+#include <functional>
 
 /*****************************************************************************
 ** Declspecs
@@ -34,29 +38,32 @@
 /*
  * Import/exports symbols for the library
  */
-#ifdef ECL_HAS_SHARED_LIBS // ecl is being built around shared libraries
-  #ifdef kobuki_EXPORTS // we are building a shared lib/dll
-    #define kobuki_PUBLIC ECL_HELPER_EXPORT
+/*#ifdef ECL_HAS_SHARED_LIBS // ecl is being built around shared libraries
+  #ifdef KeyCart_EXPORTS // we are building a shared lib/dll
+    #define KeyCart_PUBLIC ECL_HELPER_EXPORT
     #define EXP_TEMPLATE
   #else // we are using shared lib/dll
-    #define kobuki_PUBLIC ECL_HELPER_IMPORT
+    #define KeyCart_PUBLIC ECL_HELPER_IMPORT
     #define EXP_TEMPLATE extern
   #endif
-  #define kobuki_LOCAL ECL_HELPERS_LOCAL
+  #define KeyCart_LOCAL ECL_HELPERS_LOCAL
 #else // ecl is being built around static libraries
-  #define kobuki_PUBLIC
-  #define kobuki_LOCAL
+  #define KeyCart_PUBLIC
+  #define KeyCart_LOCAL
   #define EXP_TEMPLATE
 #endif 
+*/
 
 /*****************************************************************************
 ** Namespace
 *****************************************************************************/
-
-namespace KeyCart
+namespace kobuki
 {
 
-class KeyCart_PUBLIC Cart_Command : public packet_handler::payloadBase
+   // ros::Subscriber keycart_digital_output_subscriber;
+
+
+class kobuki_PUBLIC Cart_Command : public packet_handler::payloadBase
 {
 public:
     typedef ecl::PushAndPop<unsigned char> Buffer;
@@ -64,29 +71,37 @@ public:
 
     enum Name
     {
-        Set_KeyCart_DigitalOut = 20;
+        test = 19, Set_KeyCart_DigitalOut = 20
     };
 
-    void subscribeKeyCartDigitalOutputCommand(const kobuki_msgs::DigitalOutputConstPtr);
+    struct Data
+    {
+      
+      Name command;
+
+      // KeyCart gp_out
+      uint16_t key_gpout;
+
+    }
+
+    static Cart_Command SetDigitalOutput(const DigitalOutput &digital_output, Command::Data &current_data);
 
 private:
     static const unsigned char header0;
     static const unsigned char header1;
+};
 
-
-
-}
-
-class  KeyCartRos
+class KeyCartRos
 {
 public:
-    /*********************
-    ** Ros Comms
-    **********************/
-    ros::Subscriber keycart_digital_output_subscriber;
+
+  void cart_subscribeTopics(ros::NodeHandle& nh);
+  void subscribeKeyCartDigitalOutputCommand(const kobuki_msgs::DigitalOutputConstPtr);
 
 private:
+//  void subscribeKeyCartDigitalOutputCommand(const kobuki_msgs::DigitalOutputConstPtr);
+};
 
-}
+} // namespace kobuki
 
-}
+#endif /* KEYCART_COMMAND_DATA_HPP__ */
--- src/mod.cpp
+++ src/mod.cpp
@@ -17,8 +17,8 @@
 ** Namespaces
 *****************************************************************************/
 
-//namespace kobuki {
-namespace key_cart {
+namespace kobuki
+{
 
 /*****************************************************************************
 ** Static variables initialization
@@ -31,6 +31,76 @@ const unsigned char Cart_Command::header1 = 0x55;
 ** Implementation [Command Generators]
 *****************************************************************************/
 
+
+/*
+ * Set one of the digital out pins available to the user.
+ *
+ * The LED arrays, bits 0~5
+ * - LED Red        off: 0x0000, lit: 0x0001, blink(0.5s): 0x0002, blink(2s): 0x0003
+ * - LED Yellow     off: 0x0000, lit: 0x0004, blink(0.5s): 0x0008, blink(2s): 0x000C
+ * - LED Blue       off: 0x0000, lit: 0x0010, blink(0.5s): 0x0020, blink(2s): 0x0030
+ * Melody tone, bits 6~7
+ * - tone           off: 0x0000, on : 0x0040
+ * - onRoute tone: 0x0000, warning tone: 0x0080
+ * Speed Control, bit 9
+ * - Control_1: 0x0000, Control_2: 0x0200
+ * Error Control, bit 10
+ * - indef value: 0x0000, error reset: 0x0400
+ * Movement Control
+ * - normal operation: 0x0000, emergency stop: 0x0800
+ * Dendo pin
+ * - fall:  0x0000, rise: 0x1000
+ * Electric brakes
+ * - released:  0x0000, trigered: 0x2000
+ * Robot Modes transition
+ * - standby: 0x0000, operational: 0x4000, charging:  0xC000
+ *
+ * @todo could use far better documentation here/example here.
+ *
+ * @param digital_output : mask and value to send
+ * @param current_data : need to store settings as the gp_output command is a combo command
+ * @return Command : the command to send down the wire.
+ */
+
+Command Command::Set_KeyCart_DigitalOutput(const DigitalOutput &digital_output, Command::Data &current_data)
+{
+  uint16_t values = 0x0000;
+  uint16_t clear_mask = 0xfff0;
+
+  for ( unsigned int i = 0; i < 4; ++i ) {
+    if ( digital_output.mask[i] ) {
+      if ( digital_output.values[i] ) {
+        values |= ( 1 << i );
+      }
+    } else {
+      clear_mask |= ( 1 << i ); // don't clear this bit, so set a 1 here
+    }
+  }
+  current_data.key_gpout = (current_data.key_gpout & clear_mask) | values;
+  Command outgoing;
+  outgoing.data = current_data;
+  outgoing.data.command = Command::SetDigitalOut;
+  return outgoing;
+}
+
+
+
+
+//void KeyCartRos::subscribeKeyCartDigitalOutputCommand(const kobuki_msgs::DigitalOutputConstPtr msg){
+//  DigitalOutput digital_output;
+//  for ( unsigned int i = 0; i < 4; ++i ) {
+//    digital_output.values[i] = msg->values[i];
+//    digital_output.mask[i] = msg->mask[i];
+//  }
+//  kobuki.setDigitalOutput(digital_output);
+//  return;
+
+//}
+
+//void subscribeKeyCartDigitalOutputCommand(const kobuki_msgs::DigitalOutputConstPtr msg)//
+//{
+
+//}
 
 
 } // namespace kobuki
